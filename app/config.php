b57be4c5641b0b787c0d75bd7eb518b4e384db02
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-12-14 12:13:03 +0100

    Start up config file to handle dotenv

---
+++ app/config.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . "/vendor/autoload.php";
+
+// This file packs up content in .env and makes it usable in other files, this file should be required into other files to enable usage.
+
+
+// Use this package
+use Dotenv\Dotenv;
+
+// Connect to local library & upload
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Save result to variable apiKey
+$apiKey = $_ENV['API_KEY'] ?? null;
+
+// Save result for admin
+$adminuser = $_ENV['adminuser'];
+$password = $_ENV['password'];
