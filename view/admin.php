7bf671bf5bf9e65759f5b3843369ca5926c43c12
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-12-14 12:49:49 +0100

    Organizing file structure for view files

---
+++ view/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+// To do: Build the admin site with dashboard that is shown when logged in. 
+// Here admin should be able to: 
+// - Change prices in database by prefabricated PDO-connection and query, through input and submit -> DB-query
+// - Change number of stars of the hotel
+// - Be able to buy more features 
+// - Change avaiable features on booking site
+// - Change discounts
+// - Show saldo at bank
+
+// Require in all files from app/admin
