b4e13d6ac81c826cd91be2929ac8ca46897b3170
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-12-19 11:06:00 +0100

    Removed not needed code lines in comments

---
+++ view/nav.php
@@ -0,0 +1,14 @@
+<nav>
+    <div class="navbar">
+        <a class="navLink <?php echo $_SERVER['SCRIPT_NAME'] === '/index.php' ? 'active' : ''; ?>" href="/index.php">Home</a>
+
+        <a class="navLink <?php echo $_SERVER['SCRIPT_NAME'] === '/view/admin.php' ? 'active' : ''; ?>" href="/view/admin.php">Admin</a>
+
+        <?php if (isset($_SESSION['user'])) { ?>
+            <a class="navLink" href="/app/admin/logout.php">Logout</a>
+        <?php } else { ?>
+            <a class="navLink" href="/view/admin.php">Login</a>
+        <?php } ?>
+        </ul>
+    </div>
+</nav>
