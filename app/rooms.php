940c7aebac187e8aa76308d5d2e664c898cc41cb
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-12-20 16:08:38 +0100

    Added logics to fetch occupied dates

--- app/rooms.php
+++ app/rooms.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 // require __DIR__ . "/autoload.php"; // Access to database, functions, DB-paths. Commented out, this lead to double sessions?
 
+// Get all rooms
 $roomPresentation = $pdo->prepare("SELECT * FROM rooms");
 $roomPresentation->execute();
 $rooms = $roomPresentation->fetchAll(PDO::FETCH_ASSOC);
@@ -11,5 +12,42 @@ $rooms = $roomPresentation->fetchAll(PDO::FETCH_ASSOC);
 // $roomPresentation['id'] = 1/2/3
 // $roomPresentation['room'] = budget/standard/luxuary
 // $roomPresentation['price_per_night'] = int/int/int
-// $roomPresentation['title'] = int/int/int
-// $roomPresentation['description'] = int/int/int
+// $roomPresentation['title'] = str/str/str
+// $roomPresentation['description'] = str/str/str
+
+
+// ------------------------------ Fetch occupied dates ------------------------------
+// Array for all booked dates
+$bookedByRoom = [];
+
+// Fetch all checkins for the specific room
+foreach ($rooms as $room) {
+    $booked = $pdo->prepare("SELECT arrival, departure FROM checkins WHERE room_id = :id");
+    $booked->bindParam(':id', $room['id'], PDO::PARAM_INT);
+    $booked->execute();
+    $bookedByRoom[$room['id']] = $booked->fetchAll(PDO::FETCH_ASSOC);
+}
+
+// ['arrival' => '2026-01-01 15:00', 'departure' => '2026-01-03 11:00'] --> [1, 2]
+
+function bookedDays(array $bookings): array
+{
+    $days = [];
+
+    foreach ($bookings as $b) {
+        // DB returns string, convert to DateTime 
+        $startDT = new DateTime($b['arrival']);
+        $endDT   = new DateTime($b['departure']);
+
+        // Extract day, convert to int
+        $startDay = (int)$startDT->format('j');
+        $endDay   = (int)$endDT->format('j');
+
+        // Loop through all occupied dates, exclude checkout day
+        for ($i = $startDay; $i < $endDay; $i++) {
+            $days[] = $i;
+        }
+    }
+
+    return $days;
+}
