a33ad4ffe02008b1c9970dcf28687cb1af9b5abb
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-12-20 16:15:16 +0100

    Implemented occupied dates logics to presentation block

--- view/rooms.php
+++ view/rooms.php
@@ -1,4 +1,6 @@
-    <?php foreach ($rooms as $room): ?>
+    <?php foreach ($rooms as $room):
+        $booked = bookedDays($bookedByRoom[$room['id']]);
+    ?>
         <section class="roomSection whiteBox">
             <article class="roomImg">
                 <img src="assets/images/<?= $room['room'] ?>.jpg" alt="Picture of <?= $room['room'] ?> room" />
@@ -7,6 +9,6 @@
                 <h2><?= $room['title'] ?></h2>
                 <p><?= $room['description'] ?></p>
             </article>
-            <article class="roomCalendar calendar whiteBox"><?php myCalendar($booked); ?></article>
+            <article class="roomCalendar calendar"><?php require __DIR__ . '/calendar.php'; ?></article>
         </section>
     <?php endforeach; ?>
