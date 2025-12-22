b7dc66f37495955a5f03de6de05fcbcabfb739ce
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-12-22 09:40:47 +0100

    Implemented updated variable for pdo connection to database and added code block for implementing discount in frontend, doesn't work though

--- app/totalprice.php
+++ app/totalprice.php
@@ -13,6 +13,7 @@ $selectedFeatures = $_POST['features'] ?? [];
 
 $totalRoomCost = 0;
 $totalFeatureCost = 0;
+// $discount = 0; // Discount test
 $errors = [];
 
 // ------------------ ROOM COST ------------------
@@ -27,22 +28,50 @@ if ($bookingType === 'room' && $selectedRoomId && $arrivalInput && $departureInp
             $nights = 0;
         }
 
-        $totalRoomCost = countRoomCost($pdo, $selectedRoomId, $nights);
+        $totalRoomCost = countRoomCost($pdoBooking, $selectedRoomId, $nights);
     } catch (Exception $e) {
         $errors[] = "Invalid date format.";
     }
 }
 
-// ------------------ FEATURES ------------------
+// ------------------ FEATURES COST ------------------
 if (!empty($selectedFeatures)) {
-    $totalFeatureCost = countFeatureCost($pdo, $selectedFeatures);
+    $totalFeatureCost = countFeatureCost($pdoBooking, $selectedFeatures);
 }
+
+// // //DISCOUNT TEST...
+// //  ------------------ APPLY DISCOUNT ------------------
+
+// // PREPARE DISCOUNT DATA
+
+// // Fetch luxury room id
+// $luxuryRoom = $pdoBooking->prepare("SELECT id FROM rooms WHERE room = 'luxury'");
+// $luxuryRoom->execute();
+// $luxuryRoom = $luxuryRoom->fetch(PDO::FETCH_ASSOC);
+// $luxuryRoomId = $luxuryRoom['id']; // 3
+
+// // Fetch Bowser feature id
+// $bowserId = $pdoBooking->prepare('SELECT id FROM features WHERE feature LIKE "Bowser%"');
+// $bowserId->execute();
+// $bowserId = $bowserId->fetch(PDO::FETCH_ASSOC);
+// $bowserFeatureId = $bowserId['id']; // 16
+
+// // Fetch combo discount
+// $discountCombo = $pdoAdmin->prepare(
+//     "SELECT discount FROM discounts WHERE type = 'luxuryCombo'"
+// );
+// $discountCombo->execute();
+// $comboDiscount = $discountCombo->fetch(PDO::FETCH_ASSOC);
+// $comboDiscount = $comboDiscount['discount']; // 5
+
+// if (isset($selectedRoomId) && $selectedRoomId === $luxuryRoomId && in_array($bowserFeatureId, $selectedFeatures, true)) {
+//     $discount = $comboDiscount;
+// };
 
 // ------------------ TOTAL PRICE ------------------
 $totalPrice = $totalRoomCost + $totalFeatureCost;
 
 // ------------------ RETURN JSON ------------------
-header('Content-Type: application/json; charset=utf-8');
 echo json_encode([
     'totalPrice' => $totalPrice,
     'errors' => $errors
