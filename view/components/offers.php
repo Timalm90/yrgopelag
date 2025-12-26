13534847e28ece33101d231873835a1865db769f
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-12-26 19:13:41 +0100

    Created offers component

---
+++ view/components/offers.php
@@ -0,0 +1,4 @@
+<div class="stickyOffers">
+    <p>🏰 <strong>Luxury Combo:</strong> Book Princess Peach Suite (Luxury Room) + Bowser's Castle Escape and get -5c discount!</p>
+    <p>⭐ <strong>Loyal guests:</strong> Get -2c on Princess Peach Suite (Luxury Room) <strong>(applied after booking)</strong></p>
+</div>
