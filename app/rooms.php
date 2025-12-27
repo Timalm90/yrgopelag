ec158b584435139e046e9d9162d1aacb28b99abc
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-12-27 09:13:47 +0100

    Implemented function

--- app/rooms.php
+++ app/rooms.php
@@ -4,17 +4,7 @@ declare(strict_types=1);
 // require __DIR__ . "/autoload.php"; // Access to database, functions, DB-paths. Commented out, this lead to double sessions?
 
 // Get all rooms
-$roomPresentation = $pdoBooking->prepare("SELECT * FROM rooms");
-$roomPresentation->execute();
-$rooms = $roomPresentation->fetchAll(PDO::FETCH_ASSOC);
-
-// Cheatsheet:
-// $roomPresentation['id'] = 1/2/3
-// $roomPresentation['room'] = budget/standard/luxuary
-// $roomPresentation['price_per_night'] = int/int/int
-// $roomPresentation['title'] = str/str/str
-// $roomPresentation['description'] = str/str/str
-
+$rooms = getRooms($pdoBooking);
 
 // ------------------------------ Fetch occupied dates ------------------------------
 // Array for all booked dates
