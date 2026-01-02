8a123a3a66ae624a75d651ff1edbce6395a2c9e5
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-01-02 21:02:48 +0100

    Changed to .png in room image

--- view/components/rooms.php
+++ view/components/rooms.php
@@ -3,7 +3,7 @@
     ?>
         <section class="roomSection whiteBox">
             <article class="roomImg">
-                <img src="assets/images/<?= htmlspecialchars($room['room']) ?>.jpeg" alt="Picture of <?= htmlspecialchars($room['room']) ?> room" />
+                <img class="roomImg" src="assets/images/<?= htmlspecialchars($room['room']) ?>.png" alt="Picture of <?= htmlspecialchars($room['room']) ?> room" />
             </article>
             <article class="roomDescription">
                 <h2><?= htmlspecialchars($room['title']) ?></h2>
