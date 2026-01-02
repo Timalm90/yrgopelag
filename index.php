fa95a2ddd82b7a36f7d9e61233285f7bd4fd8d3b
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-01-02 21:01:32 +0100

    Moved confirmation modal into a component file and required it in

--- index.php
+++ index.php
@@ -101,62 +101,13 @@ $starRating = (int) $starRating;
     ?>
 
     <?php require __DIR__ . "/view/components/featureModal.php"; ?>
-    <!-- ------------------------------------------ SUCCESS MODAL ------------------------------------------ -->
+    <!-- ------------------------------------------ CONFIRMATION MODAL ------------------------------------------ -->
     <?php
-    if (!empty($_SESSION['success'])) { ?>
-        <section class="modal">
-            <article class="whiteBox">
-                <?php $confirmation = $_SESSION['success']; ?>
-                <h2>Dear <?= ucwords(htmlspecialchars($confirmation['visitor'])) ?>,</h2>
-                <p>Thank you for choosing <?= ucwords(htmlspecialchars(getSettingsValue($pdoBooking, 'hotel_name'))) ?> on <?= ucwords(htmlspecialchars(getSettingsValue($pdoBooking, 'island_name'))) ?>. We're looking forward to your visit!</p>
+    if (!empty($_SESSION['success'])) {
+        $confirmation = $_SESSION['success'];
 
-                <!-- Date info -->
-                <?php if ($confirmation['bookingType'] === "Day pass"): ?>
-                    <p>
-                        Your Day pass is valid for <strong><?= htmlspecialchars($confirmation['arrival']) ?></strong>.
-                    </p>
+        require __DIR__ . "/view/components/confirmationModal.php";
 
-                <?php else: ?>
-                    <p>
-                        Your visit is registered for <?= htmlspecialchars($confirmation['arrival']) ?> - <?= htmlspecialchars($confirmation['departure']) ?>.<br>
-                        Check-in: <?= htmlspecialchars($confirmation['checkinTime']) ?><br>
-                        Checkout: <?= htmlspecialchars($confirmation['checkoutTime']) ?>
-                    </p>
-                <?php endif ?>
-
-                <!-- Room info -->
-                <?php if ($confirmation['roomName']): ?>
-                    <p>
-                        Your room: <strong><?= ucwords(htmlspecialchars($confirmation['roomName'])) ?></strong>
-                    </p>
-                <?php endif ?>
-
-                <!-- Features -->
-                <?php if (!empty($confirmation['features'])): ?>
-                    <p>Included features: </p>
-                    <ul>
-                        <?php foreach ($confirmation['features'] as $feature): ?>
-                            <li>
-                                <?= ucwords(htmlspecialchars($feature)) ?>
-                            </li>
-                        <?php endforeach ?>
-                    </ul>
-                <?php endif ?>
-
-                <!-- Price -->
-                <p>Total price: <?= htmlspecialchars($confirmation['totalcost']) ?> credits</p>
-                <?php if (!empty($confirmation['discountSum'])): ?>
-                    <p>
-                        You saved <?= htmlspecialchars($confirmation['discountSum']) ?> credits.
-                    </p>
-                <?php endif ?>
-
-                <button class="modalClose">&times;</button>
-
-            </article>
-        </section>
-
-    <?php
         //Empty this session variable
         $_SESSION['success'] = NULL;
     }
