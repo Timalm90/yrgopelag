af45e57e437e743dec53fed6bd62225f7c5ac03b
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-01-02 23:00:37 +0100

    Toggle icons when toggling rooms

--- assets/scripts/toggleRoom.js
+++ assets/scripts/toggleRoom.js
@@ -1,14 +1,19 @@
 const buttons = document.querySelectorAll(".roomToggleBtn");
 const sections = document.querySelectorAll(".roomSection");
+const characters = document.querySelectorAll(".roomCharacter");
 
 function activateRoom(index) {
   buttons.forEach((btn, i) => {
     btn.classList.toggle("active", i === index);
-    btn.setAttribute("aria-selected", i === index);
   });
 
   sections.forEach((section, i) => {
     section.classList.toggle("active", i === index);
+  });
+
+  characters.forEach((character, charIndex) => {
+    character.classList.toggle("showCharacter", charIndex === index);
+    character.classList.toggle("hiddenCharacter", charIndex !== index);
   });
 }
 
