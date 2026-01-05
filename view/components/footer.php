fb0e9b80612ec88749af8d9f3fcb7fd040e93faa
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-01-05 20:15:26 +0100

    Start up footer

---
+++ view/components/footer.php
@@ -0,0 +1,12 @@
+<footer class="siteFooter">
+    <div class="footerContent">
+        <p>Follow us:</p>
+        <div class="socialIcons">
+            <a href="#"><img src="assets/images/fireflower.png" alt="Fire flower"></a>
+            <a href="#"><img src="assets/images/mushroomRed.png" alt="Red Mushroom"></a>
+            <a href="#"><img src="assets/images/booGhost.png" alt="Boo"></a>
+        </div>
+        <p>Made with ❤️ by Emilie for the school project Yrgopelag</p>
+
+    </div>
+</footer>
