64e4f914de5aee69362f03244a4ca4b0d892eb73
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-01-07 22:24:23 +0100

    JavaScritps for index page

---
+++ assets/scripts/index/calendar.js
@@ -0,0 +1,60 @@
+let selectedArrivalDay = null;
+let selectedDepartureDay = null;
+
+// // Clear selected days
+// function clearSelection() {
+//   document.querySelectorAll(".day.selected").forEach((d) => {
+//     d.classList.remove("selected");
+//   });
+// }
+
+// Event listener on calendar
+document.querySelectorAll(".roomWrapper .day").forEach((day) => {
+  day.addEventListener("click", () => {
+    // User can't choose booked dates
+    if (day.classList.contains("booked")) return;
+
+    const roomId = day.dataset.room;
+    const date = day.dataset.date;
+    const clickedDate = new Date(date);
+
+    const arrivalInput = document.querySelector("#arrivalDate");
+    const departureInput = document.querySelector("#departureDate");
+
+    // Select room in form
+    const roomInput = document.querySelector(`#room_${roomId}`);
+    if (roomInput) roomInput.checked = true;
+
+    // 1st click = arrival. If arrival > departure -> new arrival.
+    if (!arrivalInput.value || clickedDate < new Date(arrivalInput.value)) {
+      arrivalInput.value = date;
+      if (departureInput) departureInput.value = "";
+      selectedArrivalDay = day;
+      selectedDepartureDay = null;
+
+      clearSelection();
+      day.classList.add("selected");
+      return;
+    }
+
+    // If arrival is set, next click -> departure
+    const arrivalDate = new Date(arrivalInput.value);
+    if (clickedDate > arrivalDate) {
+      departureInput.value = date;
+      selectedDepartureDay = day;
+
+      clearSelection();
+
+      // Highlight choosen dates
+      selectedArrivalDay.classList.add("selected");
+      selectedDepartureDay.classList.add("selected");
+
+      // Update dynamic total price
+      updateTotalPrice();
+
+      // Scroll to form
+      const form = document.querySelector(".bookingForm");
+      if (form) form.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
---
+++ assets/scripts/index/generateTransferCode.js
@@ -0,0 +1,50 @@
+const getButton = document.querySelector("#getTransferCode");
+const errorMessage = document.querySelector("#transferCodeError");
+
+getButton.addEventListener("click", () => {
+  const name = document.querySelector("#name").value;
+  const apiKey = document.querySelector("#apiKey").value;
+
+  //Reset error
+  errorMessage.textContent = "";
+  errorMessage.classList.remove("hint");
+
+  // Validate name & apiKey
+  if (!name || !apiKey) {
+    errorMessage.textContent = "Please fill in your name and API key";
+    errorMessage.classList.add("hint");
+    return;
+  }
+
+  // Fetch total price from frontend
+  const totalCost =
+    parseInt(document.querySelector("#totalPrice").textContent) || 0;
+
+  if (totalCost <= 0) {
+    errorMessage.textContent =
+      "Total price is 0, please choose room and/or features to complete your order and try again!";
+    errorMessage.classList.add("visible", "hint");
+    return;
+  }
+
+  // --- Send request to backend ---
+  fetch("app/getTransferCode.php", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, apiKey, amount: totalCost }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.transferCode) {
+        document.querySelector("#transferCode").value = data.transferCode;
+      } else {
+        errorMessage.textContent = data.error;
+        errorMessage.classList.add("hint");
+      }
+    })
+    .catch((err) => {
+      // console.error(err);
+      errorMessage.textContent = "Failed to get transfer code";
+      errorMessage.classList.add("hint");
+    });
+});
