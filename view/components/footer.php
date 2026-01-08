5528315a050c282eb1539d3545ef13dfd380c274
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-01-08 17:22:37 +0100

    Updated footer with content

--- view/components/footer.php
+++ view/components/footer.php
@@ -1,12 +1,32 @@
-<footer class="siteFooter">
-    <div class="footerContent">
-        <p>Follow us:</p>
-        <div class="socialIcons">
-            <a href="#"><img src="assets/images/fireflower.png" alt="Fire flower"></a>
-            <a href="#"><img src="assets/images/mushroomRed.png" alt="Red Mushroom"></a>
-            <a href="#"><img src="assets/images/booGhost.png" alt="Boo"></a>
-        </div>
-        <p>Made with ❤️ by Emilie for the school project Yrgopelag</p>
+<footer>
+    <img class="eggIcon" src="assets/images/yoshiEgg.png" alt="Yoshi Egg" />
 
-    </div>
+    <article class="contactArticle">
+        <div>
+            <h3>Address:</h3>
+            <p>Rainbow Street 1</p>
+            <p>Starlight Island</p>
+        </div>
+
+        <div>
+            <h3>Contact: </h3>
+            <p>++46 70 123 45 67</p>
+            <p>yoshisresort@hotel.com</p>
+        </div>
+    </article>
+
+    <article class="socialsArticle">
+        <img class="bellIcon" src="assets/images/bell.png" alt="Super Mario Bell" />
+        <div class="socials">
+            <h3>Follow us:</h3>
+        </div>
+        <div class="socialIcons">
+            <a href="#"><img class="socialIcon" src="assets/images/fireflower.png" alt="Fire flower"></a>
+            <a href="#"><img class="socialIcon" src="assets/images/mushroomRed.png" alt="Red Mushroom"></a>
+            <a href="#"><img class="socialIcon" src="assets/images/booGhost.png" alt="Boo"></a>
+        </div>
+        </div>
+    </article>
+
+    <img class="goombas" src="assets/images/goombaPile.png" alt="A bunch of Goombas" />
 </footer>
